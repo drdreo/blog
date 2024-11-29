d671cde33f14e8b1dbb6b150ae39e20f711e2e44
DrDreo <code@drdreo.com>
2024-11-29 14:39:18 +0100

    more styling on index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,19 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     standalone: true,
     imports: [RouterLink, RouterOutlet],
     template: `
-        <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
-            <nav class="flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10">
-                <a routerLink="/">Home</a>
+        <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0 relative">
+            <nav
+                class="flex items-center w-full sticky py-10 top-10 justify-between rounded-lg transition-colors -mx-20"
+            >
+                <a
+                    routerLink="/"
+                    class="text-xl font-thin  text-gray-800 dark:text-gray-100 hover:text-amber-400 transition-colors"
+                >
+                    Home
+                </a>
             </nav>
 
-            <main class="mb-auto">
+            <main class="mt-8">
                 <router-outlet />
             </main>
         </section>
@@ -20,11 +27,6 @@ import { RouterLink, RouterOutlet } from '@angular/router';
         :host {
             max-width: 1280px;
             margin: 0 auto;
-        }
-
-        nav {
-            text-align: left;
-            padding: 0 0 2rem 0;
         }
     `,
 })
